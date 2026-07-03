affadf439d9e4e57aa466e3cdc7b90785d21dc80
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-03 20:49:40 +0530

    input and output day 3

---
+++ 3.py
@@ -0,0 +1,10 @@
+print("rahulla") ## output
+age=input("age: ")
+boy_name=input("boy name:")
+boy_age=int(input("boy age:"))
+girl_name=input("girl name:")
+girl_age=int(input("girl age:"))
+print(boy_name+ " hates " + girl_name)
+age_diffrence=boy_age-girl_age
+print(boy_name+ " hates " + girl_name + " because of age difference of " + str(age_diffrence) + " years")
+
