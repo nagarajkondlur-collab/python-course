2977f50e73db132b0de3c5acbafbe207ba2c0f40
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-06 10:21:18 +0530

    added 4 input-output problems

--- problems/problems4.py
+++ problems/problems4.py
@@ -1,1 +1,5 @@
-# problem 4 :
+# problem 4 : add two numbers
+num1 = float(input("enter first number: "))
+num2 = float(input("enter second number: "))
+sum = num1+ num2
+print("sum of two numbers is: ", sum)
