36443e8d75dabc6e8ba831b606f75b893219fea9
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-09 19:34:06 +0530

    Add comparison operators examples to demonstrate equality and relational checks

--- 4.py
+++ 4.py
@@ -12,3 +12,15 @@ a = 10
 b = 100
 a *= b
 print(a)
+
+# operators-comparision
+a = 20
+b = 30
+print(a==b) # to  check a and b are equal or not
+print(a != b) # this is used to check variables are not equal
+print(a>b)
+print(a<b)
+print(a<10)
+print(a<=25)
+print(a>=25)
+
