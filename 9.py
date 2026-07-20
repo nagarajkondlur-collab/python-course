7de00bf08ac46b69054559817b09be599eb7e66d
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-20 19:10:45 +0530

    Add while loops examples and implement PIN validation

---
+++ 9.py
@@ -0,0 +1,56 @@
+# TOPIC :while loops
+from ast import Continue
+
+
+is_failed = True
+i = 1
+
+while is_failed and i <= 99: ## fail agidre try again anta print madu,matte specific number gay star madu
+    print(f"try again {i}")
+    i = i + 1 # usually i one agirute , i=i+1 andre 2 aguttte next 3 aguute hage
+
+print("i gave up and start other")
+
+nagarj_never_give_up = True
+i = 1
+while nagarj_never_give_up:
+    if i%2!=0:# is not even ,even number matra print madu
+      i= i+1 # i%2!=0 andre i prcentage 2 not equaal to 0 badre evan alla
+      continue #continue madu
+    print(f"dont lose hopes{i}")
+    i= i+1
+    if i>100:# loop na break madu anta
+       break
+print(" i will quit")
+print("all is well")
+
+# continue statement in while loop:
+i = 0 
+while i<=20:
+  print(i*"nagaraj")#zero print madu anta otherwise i+1 haki upto kottiro number thanka print madu anta
+  i = i+1
+  
+# while in while(nested while loops)
+i = 1
+while i<=10:
+  x = 0 
+  while x<i:
+     print("nagaraj", end="-") # used to end it
+     x= x+1
+  print("")
+  i = i+1
+  
+  # some  real world examples
+  
+pin = "1234"
+input_pin = input("pin -") 
+  
+if input_pin =="pin":
+   print("CORRECT")
+else:
+   print("INCORRECT")
+    
+  
+  
+  
+
