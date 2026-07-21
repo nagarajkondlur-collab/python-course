ccf22019081ef93f8c188317960e256d85aeccea
NAGARAJ .YS <nagaraj.kondlur@gmail.com>
2026-07-21 13:08:59 +0530

    Add Python examples for PIN validation and user input handling

---
+++ problems/problems28.py
@@ -0,0 +1,31 @@
+ # some  real world examples
+  
+pin = "1234"
+trails = 1
+while trails<=3:
+    input_pin = input(f"Trail-{trails}|pin -")
+    trails = trails+1
+    if input_pin == pin:
+        print("CORRECT")
+        break # correct adhmele stop madu matte kelbeda anta
+    else:
+        print("INCORRECT")
+        
+        
+        break
+    
+        
+        
+        
+# examples 1.
+name = ""
+
+while name == "":
+    name = input("enter your name dear = ")
+    print(f"hello{name}")
+    
+
+
+        
+   
+   
